05e58e3614c4f1ba98482cbfdfe5c5fbc421f791
adamprobert <adamprobert@live.co.uk>
2020-01-31 23:32:13 +0000

    Added initial ui for builder

---
+++ GGJ2020/Assets/Scripts/Component.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Component
+{
+
+    public Sprite Icon;
+    public string Name;
+
+    public Component(Sprite icon, string name)
+    {
+        Icon = icon;
+        Name = name;
+    }
+}
