6476e6b11688092e4a1fceb2fdbed8b2648eb0a4
Sam <lord.samuel.barton@gmail.com>
2020-02-01 12:06:02 +0000

    Added in attack method and spin axe

--- GGJ2020/Assets/Scripts/Weapons/Axe.cs
+++ GGJ2020/Assets/Scripts/Weapons/Axe.cs
@@ -7,13 +7,32 @@ public class Axe : BaseMelee // Derived class
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        attack();
+    }
+
+    public override void attack(){
+        Debug.Log("ATTACKING");
+        Debug.Log("last attack time: " + lastAttackTime);
+        Debug.Log("Time Since last attack: " + (attackTimeTracker + Time.deltaTime));
+        Debug.Log("Next Attack time: " + (lastAttackTime));
+
+        if ((attackTimeTracker + Time.deltaTime) > (lastAttackTime)){
+            //this.object apply force after waiting the swing delay
+            rigidBody.AddTorque(0,0,-10000);
+            lastAttackTime = Time.time + attackCooldown;
+            Debug.Log("Inside the if");
+        } else{
+            //Do nothing, otherwise we buffer attacks forever
+             attackTimeTracker += Time.deltaTime;
+        }
+
     }
 
 }
--- GGJ2020/Assets/Scripts/Weapons/BaseMelee.cs
+++ GGJ2020/Assets/Scripts/Weapons/BaseMelee.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseMelee : MonoBehaviour
+public abstract class BaseMelee : MonoBehaviour
 {
-
-    public int damage;
-    private double lastAttackTime;
+    public double weaponHealth;
+    public double damage;
+    public double lastAttackTime;
+    public double attackTimeTracker;
     public double swingDelay;
     public double attackCooldown = 0.5;
     public double swingSpeed;
+    
+    public Rigidbody rigidBody;
 
     // Start is called before the first frame update
     void Start(){
         lastAttackTime = 0.00;
+
     }
 
     // Update is called once per frame
@@ -21,16 +25,9 @@ public class BaseMelee : MonoBehaviour
         attack();
     }
 
-    void attack(){
-        if ((lastAttackTime + Time.deltaTime) > (lastAttackTime + attackCooldown)){
-            //this.object apply force after waiting the swing delay
-            this.rigidBody.AddRelativeTorque(100);
-        } else{
-            //Do nothing, otherwise we buffer attacks forever
-        }
-    }
+    public virtual void attack(){}
 
-    void hitTarget(){
+    public void hitTarget(){
         // call damage step to networking.
     }
 }
