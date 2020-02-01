9833ae1b9f8a619ea7efce09eedb96f883ddc40a
Sam Barton <lord.samuel.barton@gmail.com>
2020-02-01 10:33:20 +0000

    Axe script starter

---
+++ GGJ2020/Assets/Scripts/Weapons/BaseMelee.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMelee : MonoBehaviour
+{
+
+    public int damage;
+    private double lastAttackTime;
+    public double swingDelay;
+    public double attackCooldown = 0.5;
+    public double swingSpeed;
+
+    // Start is called before the first frame update
+    void Start(){
+        lastAttackTime = 0.00;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        attack();
+    }
+
+    void attack(){
+        if ((lastAttackTime + Time.deltaTime) > (lastAttackTime + attackCooldown)){
+            //this.object apply force after waiting the swing delay
+            this.rigidBody.AddRelativeTorque(100);
+        } else{
+            //Do nothing, otherwise we buffer attacks forever
+        }
+    }
+
+    void hitTarget(){
+        // call damage step to networking.
+    }
+}
