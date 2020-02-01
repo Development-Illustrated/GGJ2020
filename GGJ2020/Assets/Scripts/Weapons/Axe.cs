7d4a7efbb74904aa400529a9415ad2e8345869e7
Sam <lord.samuel.barton@gmail.com>
2020-02-01 15:11:50 +0000

    added in base function for axe prefab

--- GGJ2020/Assets/Scripts/Weapons/Axe.cs
+++ GGJ2020/Assets/Scripts/Weapons/Axe.cs
@@ -2,37 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Axe : BaseMelee // Derived class
+public class Axe : BaseWeapon // Derived class
 {
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        
+        hinge = GetComponent<HingeJoint>();
+        motor = hinge.motor;
+        //TODO: ROBOT CONENCTION POINT
+        // hinge.connectedBody = rigidBody;
     }
 
     // Update is called once per frame
     void Update()
     {
-        attack();
+        checkWeaponPosition();
+        attackTimeTracker += Time.deltaTime;
     }
 
+    /*
+    Attack function checks if the weapon has attack recently before making the action.
+    */
     public override void attack(){
-        Debug.Log("ATTACKING");
-        Debug.Log("last attack time: " + lastAttackTime);
-        Debug.Log("Time Since last attack: " + (attackTimeTracker + Time.deltaTime));
-        Debug.Log("Next Attack time: " + (lastAttackTime));
-
-        if ((attackTimeTracker + Time.deltaTime) > (lastAttackTime)){
-            //this.object apply force after waiting the swing delay
-            rigidBody.AddTorque(0,0,-10000);
-            lastAttackTime = Time.time + attackCooldown;
-            Debug.Log("Inside the if");
-        } else{
-            //Do nothing, otherwise we buffer attacks forever
-             attackTimeTracker += Time.deltaTime;
+        if ((attackTimeTracker + Time.deltaTime) > (nextMinAttackTime)){
+            //Activated motor to attack, and sets next possible time for an attack
+            hinge.useMotor = true;
+            nextMinAttackTime = Time.time + attackCooldown;
         }
-
     }
 
+    private void checkWeaponPosition(){
+        //Once the axe is past a certain point swap from motor to spring
+        if(hinge.angle >= hinge.limits.max ){
+            hinge.useMotor = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        //calculate damage by modifier and speed
+        base.hitTarget(collision.gameObject, this.damage);
+
+    }
 }
