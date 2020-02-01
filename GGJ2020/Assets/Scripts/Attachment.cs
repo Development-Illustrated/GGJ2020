711a104a357331e3968c7e065df5c70e8b43f9d3
adamprobert <adamprobert@live.co.uk>
2020-02-01 10:27:18 +0000

    Basic attachment working

--- GGJ2020/Assets/Scripts/Attachment.cs
+++ GGJ2020/Assets/Scripts/Attachment.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Attachment : MonoBehaviour
 {
     [SerializeField] Transform attachPoint;
+    [SerializeField] Vector3 postSpawnRotation; // Used to alter the rotation after spawning on attachpoint
     public void Attach(Transform attachPoint)
     {
         this.attachPoint = attachPoint;
         transform.position=attachPoint.position;
         transform.rotation=attachPoint.rotation;
+        transform.parent = attachPoint;
+
+        Quaternion target = Quaternion.Euler(postSpawnRotation);
+        transform.rotation = target;
+
+        attachPoint.GetComponent<MeshRenderer>().enabled = false;
     }
 }
