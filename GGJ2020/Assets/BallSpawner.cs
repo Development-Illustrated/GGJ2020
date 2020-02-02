d9bd526cb84f7297a6f12feb2528d3feee9f9dcc
luketurner9@gmail.com <luketurner9@gmail.com>
2020-02-02 11:42:16 +0000

    more ballz

--- GGJ2020/Assets/BallSpawner.cs
+++ GGJ2020/Assets/BallSpawner.cs
@@ -10,10 +10,14 @@ public class BallSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        float Scale;
         //InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
         while (ballNumber > 1){
             GameObject newBall = Instantiate(ball, new Vector3(Random.Range(-34f,-15f), 1, Random.Range(40f,115f)), Quaternion.identity);
             newBall.GetComponent<Renderer>().material.SetColor("_Color", Random.ColorHSV());
+            //Scale = Random.Range(0.5f,2.5f);
+            Scale = 2f;
+            newBall.GetComponent<Transform>().localScale = new Vector3(Scale,Scale,Scale);
             ballNumber -= 1;
         }
 
