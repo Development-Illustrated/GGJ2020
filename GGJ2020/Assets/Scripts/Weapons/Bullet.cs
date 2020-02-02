6248533bed7eb659a522edc79159169d51d26d39
Daniel Neenan <daniel@automise.co.uk>
2020-02-02 16:40:06 +0000

    Remove bullet fluff

--- GGJ2020/Assets/Scripts/Weapons/Bullet.cs
+++ GGJ2020/Assets/Scripts/Weapons/Bullet.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float lifeTime = 0f;
-    public float maxLifeTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +12,10 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lifeTime += Time.deltaTime;
-        Debug.Log("Gun bullet lifeTime" + lifeTime);
-        if (lifeTime > maxLifeTime) {
-            lifeTime = 0;
-            killBill();
-        }
     }
 
     void OnCollisionEnter(Collision col)
     {
         Debug.Log("Gun bullet collision with" + col.gameObject.tag);
-        killBill();
-    }
-
-    void killBill() {
-        lifeTime = 0;
-        DestroyImmediate(this);
     }
 }
